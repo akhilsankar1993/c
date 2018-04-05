e376373c02ea52ee9f0b54f3e35ef7058861197f
akhilsankar1993 <akhil.sankar@wink.com>
2018-04-05 09:09:49 -0400

    ex10 (v3) - handle multiple args and uses tolower()

--- ex10.c
+++ ex10.c
@@ -1,52 +1,50 @@
 #include <stdio.h>
+#include <ctype.h>
 
 int main(int argc, char* argv[])
 {
-    if (argc != 2) {
+    if (argc == 1) {
         printf("ERROR: you need atleast one argument \n");
         //this is the code to return when you need to abort a program.
         return 1;
     }
 
-    for (int i = 0; argv[1][i] != '\0'; i++) {
-        char letter = argv[1][i];
+    for (int j = 1; j < argc; j++) {
+        for (int i = 0; argv[j][i] != '\0'; i++) {
+            char letter = tolower(argv[j][i]);
 
-        switch (letter) {
-            case 'a':
-            case 'A':
-                printf("%d: 'A'\n", i);
-                break;
+            switch (letter) {
+                case 'a':
+                    printf("%d: 'A'\n", i);
+                    break;
 
-            case 'e':
-            case 'E':
-                printf("%d: 'E'\n", i);
-                break;
+                case 'e':
+                    printf("%d: 'E'\n", i);
+                    break;
 
-            case 'i':
-            case 'I':
-                printf("%d: 'I'\n", i);
-                break;
+                case 'i':
+                    printf("%d: 'I'\n", i);
+                    break;
 
-            case 'o':
-            case 'O':
-                printf("%d: 'O'\n", i);
-                break;
+                case 'o':
+                    printf("%d: 'O'\n", i);
+                    break;
 
-            case 'u':
-            case 'U':
-                printf("%d: 'U'\n", i);
-                break;
+                case 'u':
+                    printf("%d: 'U'\n", i);
+                    break;
 
-            case 'y':
-            case 'Y':
-                if (i > 2) {
-                  printf("%d, 'Y'\n", i);
-                }
-                break;
+                case 'y':
+                    if (i > 2) {
+                      printf("%d, 'Y'\n", i);
+                    }
+                    break;
 
-            default:
-                printf("%d: %c is not a vowel\n", i, letter);
+                default:
+                    printf("%d: %c is not a vowel\n", i, letter);
+            }
         }
     }
+
     return 0;
 }
