6af059e1ce5157ba6d87e6802e2551b362275c34
Akhil Sankar <akhilsankar@Akhils-MBP.home>
2018-09-03 12:14:17 -0400

    adding db connection

--- ex17.c
+++ ex17.c
@@ -5,32 +5,65 @@
 #define MAX_DATA 512
 #define MAX_ROWS 100
 
-struct Database = {
-  struct Address rows[MAX_ROWS];
+struct Address {
+    int id;
+    int set;
+    char name[MAX_DATA];
+    char email[MAX_DATA];
 }
 
-struct Connection = {
-  FILE *file;
-  struct Database *db;
+struct Database {
+    struct Address rows[MAX_ROWS];
+}
+
+struct Connection {
+    FILE *file;
+    struct Database *db;
+}
+
+void Database_create(struct Connection *conn) {
+    int i = 0;
+
+    for(i = 0; i < MAX_ROWS; i++) {
+      struct Address addr = { .id = i, .set = 0 };
+      conn->db->rows[i] = addr;
+    }
 }
 
 struct Connection *Database_open(const char *filename, char mode) {
-  struct Connection *conn = malloc(sizeof(struct Connection))
+    struct Connection *conn = malloc(sizeof(struct Connection))
 
-  if(!conn) {
-    // error
-  }
+    if(!conn) {
+      // memory error!
+    }
 
-  conn->db = malloc(sizeof(struct Database));
+    conn->db = malloc(sizeof(struct Database));
+    if(!conn->db) {
+      // memory error!
+    }
+
+    if(mode == 'c') {
+        // set file attribute as opened file, with streaming positioned at the
+        // beginning of the file, for writing.
+        conn->file = fopen(filename, 'w');
+    } else {
+        // open file for reading and writing here.
+        conn->file = fopen(filename, 'r+');
+
+        if(conn->file) {
+            Database_load(conn);
+        }
+    }
+
+
 
 }
 
 int main(int argc, char *argv[]) {
+    char *filename = argv[1];
+    char action = argv[2][0];
 
-  char *filename = argv[1];
-  char action = argv[2][0];
+    struct Connection *conn = Database_open(filename, action);
 
-  struct Connection *conn = Database_open(filename, action);
-
-  return 0;
+    return 0;
 }
